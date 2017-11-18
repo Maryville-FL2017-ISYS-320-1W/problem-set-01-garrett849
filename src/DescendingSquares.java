30150f63b6598ec2c8d6ebfe98e539014cebf500
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-18 13:59:35 -0600

    DescendingSquares

--- src/DescendingSquares.java
+++ src/DescendingSquares.java
@@ -5,14 +5,19 @@
 */
 
 /* 2. Your pseudocode algorithm here
-
+	
+	Make a loop that descends from 10 to 1
+	Print out the number 10 to 1 multiply by it self then a space.
 
 */
 
 public class DescendingSquares {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		for (int i = 10; i >= 1; i--) {
+			System.out.print(i * i + " ");
+		}
 
 	}
 
