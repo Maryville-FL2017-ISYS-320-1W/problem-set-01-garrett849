36a21e91ea7876716a8ea62d8e3a2a9510ecd88d
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-18 14:20:46 -0600

    NestedLoopBox

--- src/NestedLoopBox.java
+++ src/NestedLoopBox.java
@@ -5,14 +5,21 @@
 */
 
 /* 4. Your pseudocode algorithm here
-
+     Write a loop that will produce 6 stars in a line.
+     Write another loop that will produce the above loop 6 times on 6 separate lines below. 
+     println
+     End
 
 */
 public class NestedLoopBox {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for (int line = 1; line <= 6; line++) {
+			for (int star = 1; star <= 6; star++) {
+				System.out.print("*");
+			}
+			System.out.println();
 
+		}
 	}
-
 }
