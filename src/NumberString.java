ba9bf7bbfa7fb63a6d03123219fb6d183c9ecc04
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-18 15:33:11 -0600

    NumberString

--- src/NumberString.java
+++ src/NumberString.java
@@ -5,15 +5,26 @@
 */
 
 /* 6. Your pseudocode algorithm here
-
+	Print a loop that will loop 3 times.
+	Print a nested loop under first loop that will produces 0 thru 9.
+	Print a nested loop under second loop that produces upper loop 3 times
+	Println.
+	End
 
 */
 
 public class NumberString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j <= 9; j++) {
+				for (int k = 0; k < 3; k++) {
+					System.out.print(j);
+				}
+
+			}
+			System.out.println();
+		}
 	}
-
 }
