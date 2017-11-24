0f571232c25f71881ce35191c8c45eebbcdd4472
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-24 12:00:51 -0600

    FollowTheLoop

--- src/FollowTheLoop.java
+++ src/FollowTheLoop.java
@@ -9,17 +9,19 @@
 1. Fill out the table for the loop below
 
 | ITERATION 		 | VARIABLE NAME	 | VARIABLE NAME	 | VARIABLE NAME	 |
-| initialization	 |				 |				 |				 | 
-| 1				 |				 |				 |				 |
-
+| initialization	 | MAX			     | count			 |		number		 | 
+| 1					 |		5		     |			5	     |		26		     |
+| 2                  |      6            |          6        |      38           |
+| 3                  |      6            |          6        |      39           |
+| 10                 |      6            |          6        |      46           |
 
 */
 
 public class FollowTheLoop {
 
-	   public static final int MAX = 4;
+	   public static final int MAX = 6;
 	   public static void loopDeDoop() {
-	      int number = 0;
+	      int number = 10;
 	      for( int count = MAX; count >= 1; count-- ) {
 	         int product = count * 2 - 1;
 	         number = number + product;
